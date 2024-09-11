8c0d21ceddf5a731594536523d6eb26c2b7321d4
Aorathai27 <150641735+Aorathai27@users.noreply.github.com>
2024-09-11 21:39:34 +0700

    01

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTab from './navigations/BottomTab';
+import FlatListExample from './screens/FlatListExample';
 
 export default function App() {
   return (
-    <NavigationContainer>
-    <BottomTab />
-</NavigationContainer>
-
+//     <NavigationContainer>
+//     <BottomTab />
+// </NavigationContainer>
+<FlatListExample/>
   );
 }
 
